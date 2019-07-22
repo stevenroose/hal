4a4b2b5f5e0509b3a8fbec7fdced60b05dcbfea1
Clark Moody <clarkmoody@gmail.com>
2019-07-22 17:48:54 -0500

    Add Bech32 string to Bech32Info struct
    For decoding, this echoes back the original input string, along with
    decoded values. For encoding, the inputs are echoed back alongside the
    bech32 field output.
    
    Cargo fmt

--- src/bech32.rs
+++ src/bech32.rs
@@ -2,10 +2,12 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
 pub struct Bech32Info {
+	/// Bech32 encoded string
+	pub bech32: String,
 	/// Human-readable part
 	pub hrp: String,
 	/// Data payload as vector
-    #[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub payload_bytes: Option<Vec<u8>>,
 	/// Data payload as hex string
 	#[serde(skip_serializing_if = "Option::is_none")]
--- src/bin/hal/cmd/bech32/decode.rs
+++ src/bin/hal/cmd/bech32/decode.rs
@@ -14,29 +14,30 @@ pub fn subcommand<'a>() -> clap::App<'a, 'a> {
 }
 
 pub fn execute<'a>(matches: &clap::ArgMatches<'a>) {
-    let s = matches.value_of("string").expect("missing required argument");
-    let result = decode(&s);
-    if result.is_err() {
-        panic!("Decode failure: {:?}", result.unwrap_err());
-    }
-    let (hrp, b32_payload) = result.unwrap();
+	let s = matches.value_of("string").expect("missing required argument");
+	let result = decode(&s);
+	if result.is_err() {
+		panic!("Decode failure: {:?}", result.unwrap_err());
+	}
+	let (hrp, b32_payload) = result.unwrap();
 	let mut info = hal::bech32::Bech32Info {
+		bech32: s.to_string(),
 		hrp,
 		payload_bytes: None,
 		payload_hex: None,
 	};
-    let payload: Vec<u8> = if matches.is_present("convert-bits") {
-        let convert_result = Vec::<u8>::from_base32(&b32_payload);
-        if convert_result.is_err() {
-            panic!("Error converting payload to 8-bit {:?}", convert_result.unwrap_err());
-        }
-        convert_result.unwrap()
-    } else {
-        b32_payload.iter().map(|b| b.to_u8()).collect()
-    };
-    if matches.is_present("bytes") {
-        info.payload_bytes = Some(payload.to_vec());
-    }
-    info.payload_hex = Some(payload.into());
+	let payload: Vec<u8> = if matches.is_present("convert-bits") {
+		let convert_result = Vec::<u8>::from_base32(&b32_payload);
+		if convert_result.is_err() {
+			panic!("Error converting payload to 8-bit {:?}", convert_result.unwrap_err());
+		}
+		convert_result.unwrap()
+	} else {
+		b32_payload.iter().map(|b| b.to_u8()).collect()
+	};
+	if matches.is_present("bytes") {
+		info.payload_bytes = Some(payload.to_vec());
+	}
+	info.payload_hex = Some(payload.into());
 	cmd::print_output(matches, &info)
 }
