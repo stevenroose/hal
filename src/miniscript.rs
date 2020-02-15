e4458944864d8c2f89a9b4ebda90a8c696fb96fb
Steven Roose <steven@stevenroose.org>
2020-02-15 20:35:34 +0000

    Add miniscript inspect command

--- src/miniscript.rs
+++ src/miniscript.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "snake_case")]
-pub enum PolicyKeyType {
+pub enum MiniscriptKeyType {
 	PublicKey,
 	String,
 }
@@ -10,7 +10,7 @@ pub enum PolicyKeyType {
 #[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
 pub struct PolicyInfo {
 	pub is_concrete: bool,
-	pub key_type: PolicyKeyType,
+	pub key_type: MiniscriptKeyType,
 	pub is_trivial: bool,
 	pub is_unsatisfiable: bool,
 	pub relative_timelocks: Vec<u32>,
@@ -20,4 +20,16 @@ pub struct PolicyInfo {
 	pub normalized: String,
 	#[serde(skip_serializing_if = "Option::is_none")]
 	pub miniscript: Option<String>,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
+pub struct MiniscriptInfo {
+	pub key_type: MiniscriptKeyType,
+	pub script_size: usize,
+	pub max_satisfaction_witness_elements: usize,
+	pub max_satisfaction_size_segwit: usize,
+	pub max_satisfaction_size_non_segwit: usize,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub script: Option<::HexBytes>,
+	pub policy: String,
 }
